d56c6cef3d12cfb360506a3134753c4561318b94
Frank Ngo <frankngomusic@gmail.com>
2018-03-16 13:42:58 -0700

    add test for age to mercurian age

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -1,17 +1,30 @@
-import { Person } from './../src/calculator.js'
+import { Calculator } from './../src/calculator.js'
 
-describe('Person', function() {
+describe('Calculator', function() {
 
-  it('creates person object with a date in format(MM/DD/YYYY)', function() {
-    let personDate;
-    personDate = new Person("6/28/1994");
-    expect(personDate.date).toEqual("6/28/1994");
+  it('creates person object with a date in format (MM/DD/YYYY)', function() {
+    let testDate = new Calculator("6/28/1994");
+    expect(testDate.date).toEqual("6/28/1994");
   });
 
   it('converts users age to seconds', function() {
     let seconds = 772786800 ;
-    let personDate = new Person("6/28/1994");
-    personDate.ageToSeconds();
-    expect(personDate.newDate).toEqual(seconds);
+    let testDate = new Calculator("6/28/1994");
+    testDate.ageToSeconds();
+    expect(testDate.newDate).toEqual(seconds);
+  });
+
+  it('compares users age in seconds with current date in seconds', function() {
+    let testDate = new Calculator("6/28/1994");
+    let currentDate = Math.floor(Date.now() / 1000);
+    let differenceBetweenDates = currentDate - testDate.newDate;
+    expect(testDate.dateDifference()).toEqual(differenceBetweenDates);
+  });
+
+  it('convertrs and returns users age in mercurian  years', function() {
+    let testDate = new Calculator("6/28/1994");
+    let age = testDate.dateDifference();
+    let result = age/(365 * .24);
+    expect(testDate.mercurianAge()).toEqual(result);
   });
 });
--- src/calculator.js
+++ src/calculator.js
@@ -1,12 +1,23 @@
-export class Person {
+export class Calculator {
   constructor(date) {
     this.date = date;
     this.newDate = new Date();
   }
 
-
   ageToSeconds() {
     this.newDate = Date.parse(this.date)/1000;
     return this.newDate;
+  }
+
+  dateDifference() {
+     let currentDay = Math.floor(Date.now() / 1000);
+     let comparedDates = currentDay - this.newDate;
+     return comparedDates;
+  }
+
+  mercurianAge () {
+    let age = this.dateDifference();
+    let convertedAge = age/(365 * .24);
+    return convertedAge;
   }
 }
